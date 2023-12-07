206d848abe5196e84ce09f4acdf152b4d6159e0b
Jack Liu <toliujiayi@gmail.com>
2023-12-06 16:07:39 -0800

    update 20231206

--- utils/show_mesh.py
+++ utils/show_mesh.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import matplotlib.cm as cm
 import numpy as np
 import trimesh
 # import plotly
@@ -6,64 +8,75 @@ from chart_studio import plotly as py
 from plotly.graph_objs import *
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
-import matplotlib.cm as cm
-from functools import reduce
+
 
 def map_z2color(zval, colormap, vmin, vmax):
-    t=(zval-vmin)/float((vmax-vmin)); R, G, B, alpha=colormap(t)
-    return 'rgb('+'{:d}'.format(int(R*255+0.5))+','+'{:d}'.format(int(G*255+0.5))+','+'{:d}'.format(int(B*255+0.5))+')'   
+    t = (zval-vmin)/float((vmax-vmin))
+    R, G, B, alpha = colormap(t)
+    return 'rgb('+'{:d}'.format(int(R*255+0.5))+','+'{:d}'.format(int(G*255+0.5))+','+'{:d}'.format(int(B*255+0.5))+')'
+
 
 def tri_indices(simplices):
     return ([triplet[c] for triplet in simplices] for c in range(3))
 
+
 def plotly_trisurf(x, y, z, colors, simplices, colormap=cm.RdBu, plot_edges=False):
-    
-    points3D=np.vstack((x,y,z)).T
-    tri_vertices=map(lambda index: points3D[index], simplices)
-    
+
+    points3D = np.vstack((x, y, z)).T
+    tri_vertices = map(lambda index: points3D[index], simplices)
+
     ncolors = colors
-    if not (colors==0).all():
+    if not (colors == 0).all():
         ncolors = (colors-np.min(colors))/(np.max(colors)-np.min(colors))
-    
-    I,J,K=tri_indices(simplices)
-    triangles=Mesh3d(x=x,y=y,z=z,
-                     intensity=ncolors,
-                     colorscale='Viridis',
-                     i=I,j=J,k=K,name='',
-                     showscale=True,
-                     colorbar=ColorBar(tickmode='array', 
-                                       tickvals=[np.min(z), np.max(z)], 
-                                       ticktext=['{:.3f}'.format(np.min(colors)), 
-                                                 '{:.3f}'.format(np.max(colors))]))
-    
-    if plot_edges is False: # the triangle sides are not plotted
+
+    I, J, K = tri_indices(simplices)
+    triangles = Mesh3d(x=x, y=y, z=z,
+                       intensity=ncolors,
+                       colorscale='Viridis',
+                       i=I, j=J, k=K, name='',
+                       showscale=True,
+                       colorbar=ColorBar(tickmode='array',
+                                         tickvals=[np.min(z), np.max(z)],
+                                         ticktext=['{:.3f}'.format(np.min(colors)),
+                                                   '{:.3f}'.format(np.max(colors))]))
+
+    if plot_edges is False:  # the triangle sides are not plotted
         return Data([triangles])
     else:
-        lists_coord=[[[T[k%3][c] for k in range(4)]+[ None] for T in tri_vertices] for c in range(3)]
-        Xe, Ye, Ze=[reduce(lambda x,y: x+y, lists_coord[k], lists_coord[0]) for k in range(3)]
-        lines=Scatter3d(x=Xe,y=Ye,z=Ze,mode='lines',line=Line(color='rgb(50,50,50)', width=1.5))
+        lists_coord = [[[T[k % 3][c] for k in range(
+            4)]+[None] for T in tri_vertices] for c in range(3)]
+        Xe, Ye, Ze = [reduce(lambda x, y: x+y, lists_coord[k],
+                             lists_coord[0]) for k in range(3)]
+        lines = Scatter3d(x=Xe, y=Ye, z=Ze, mode='lines',
+                          line=Line(color='rgb(50,50,50)', width=1.5))
         return Data([triangles, lines])
+
 
 def textured_mesh(mesh, per_vertex_signal=None, samples=None, filename='visualize mesh'):
     if per_vertex_signal is None:
         per_vertex_signal = np.zeros(mesh.vertices.shape[0])
-    x = mesh.vertices.transpose()[0]; y = mesh.vertices.transpose()[1]; z = mesh.vertices.transpose()[2];
-    ran = mesh.bounds[1,:] - mesh.bounds[0,:]
-    data1 = plotly_trisurf(x, y, z, per_vertex_signal, mesh.faces, colormap=cm.RdBu,plot_edges=True)
+    x = mesh.vertices.transpose()[0]
+    y = mesh.vertices.transpose()[1]
+    z = mesh.vertices.transpose()[2]
+    ran = mesh.bounds[1, :] - mesh.bounds[0, :]
+    data1 = plotly_trisurf(x, y, z, per_vertex_signal,
+                           mesh.faces, colormap=cm.RdBu, plot_edges=True)
     if samples is not None:
         X = mesh.vertices[samples]
         ns = mesh.vertex_normals[samples]
         X += ns * 0.001
-        data2 = Scatter3d(x=X[:,0], y=X[:,1], z=X[:,2],
-                    mode='markers',
-                    marker=dict(size=3,
-                        line=dict(color='rgba(217, 217, 217, 0.14)', width=0.5),
-                        opacity=1))
+        data2 = Scatter3d(x=X[:, 0], y=X[:, 1], z=X[:, 2],
+                          mode='markers',
+                          marker=dict(size=3,
+                                      line=dict(
+                                          color='rgba(217, 217, 217, 0.14)', width=0.5),
+                                      opacity=1))
         data1.append(data2)
-    axis = dict(showbackground=True,backgroundcolor="rgb(230, 230,230)",gridcolor="rgb(255, 255, 255)",zerolinecolor="rgb(255, 255, 255)")
-    layout = Layout(width=800, height=800,scene=Scene(xaxis=XAxis(axis),yaxis=YAxis(axis),zaxis=ZAxis(axis),
-                      aspectratio=dict(x=1,y=1,z=1)),
-                   xaxis=dict(range=[mesh.bounds[0,0], mesh.bounds[1,0]]),
-                   yaxis=dict(range=[mesh.bounds[0,1], mesh.bounds[1,1]]))
+    axis = dict(showbackground=True, backgroundcolor="rgb(230, 230,230)",
+                gridcolor="rgb(255, 255, 255)", zerolinecolor="rgb(255, 255, 255)")
+    layout = Layout(width=800, height=800, scene=Scene(xaxis=XAxis(axis), yaxis=YAxis(axis), zaxis=ZAxis(axis),
+                                                       aspectratio=dict(x=1, y=1, z=1)),
+                    xaxis=dict(range=[mesh.bounds[0, 0], mesh.bounds[1, 0]]),
+                    yaxis=dict(range=[mesh.bounds[0, 1], mesh.bounds[1, 1]]))
     fig1 = Figure(data=data1, layout=layout)
     iplot(fig1, filename=filename)
--- utils/visualization_tool.py
+++ utils/visualization_tool.py
@@ -1,14 +1,14 @@
+import numpy as np
 from sklearn.manifold import MDS
 
 # import plotly.plotly as py
 from chart_studio import plotly as py
-from chart_studio.plotly import graph_objs as go
-# import plotly.graph_objs as go
+# from chart_studio.plotly import graph_objs as go
+import plotly.graph_objs as go
 # from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-from chart_studio.plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
-import numpy as np
 
 def visualize_dist_2D(dissim, savefig=False, filename=None):
     '''
@@ -21,13 +21,14 @@ def visualize_dist_2D(dissim, savefig=False, filename=None):
     # embed the data into 2D
     embedding = MDS(n_components=2, n_jobs=20, dissimilarity='precomputed')
     X = embedding.fit_transform(dissim)
-    
+
     # plot
-    trace = go.Scatter(x = X[:,0], y = X[:,1], mode = 'markers')
+    trace = go.Scatter(x=X[:, 0], y=X[:, 1], mode='markers')
     data = go.Data([trace])
     # Plot and embed in ipython notebook!
     iplot(data, filename='basic-scatter')
-    
+
+
 def visualize_dist_3D(dissim, savefig=False, filename=None):
     '''
     Visualize high dimensional data in 2D according to the dissimilarity matrix.
@@ -39,20 +40,21 @@ def visualize_dist_3D(dissim, savefig=False, filename=None):
     # embed the data into 2D
     embedding = MDS(n_components=3, n_jobs=20, dissimilarity='precomputed')
     X = embedding.fit_transform(dissim)
-    
+
     # plot
-    trace = go.Scatter3d(x=X[:,0], y=X[:,1], z=X[:,2],
-                    mode='markers',
-                    marker=dict(size=3,
-                        color='rgba(217, 217, 217, 0.14)',
-                        #line=dict(color='rgba(217, 217, 217, 0.14)', width=0.5),
-                        opacity=0.8))
+    trace = go.Scatter3d(x=X[:, 0], y=X[:, 1], z=X[:, 2],
+                         mode='markers',
+                         marker=dict(size=3,
+                                     color='rgba(217, 217, 217, 0.14)',
+                                     # line=dict(color='rgba(217, 217, 217, 0.14)', width=0.5),
+                                     opacity=0.8))
     data = go.Data([trace])
     layout = go.Layout(margin=dict(l=0, r=0, b=0, t=0))
     fig = go.Figure(data=data, layout=layout)
     iplot(fig, filename='simple-3d-scatter')
 
-def visualize_point_2D(X, axis=[0,1], labeled=None, savefig=False, filename=None):
+
+def visualize_point_2D(X, axis=[0, 1], labeled=None, savefig=False, filename=None):
     '''
     Visualize high dimensional data in 2D according by projection.
     Input:
@@ -60,23 +62,23 @@ def visualize_point_2D(X, axis=[0,1], labeled=None, savefig=False, filename=None
     Output:
     - visualization of the data in 2D
     '''
-    trace = go.Scatter(x = X[:,axis[0]], y = X[:,axis[1]],
-                 mode = 'markers',
-                 marker=dict(size=2))
+    trace = go.Scatter(x=X[:, axis[0]], y=X[:, axis[1]],
+                       mode='markers',
+                       marker=dict(size=2))
     layout = go.Layout(width=600, height=600)
     if labeled is None:
         data = go.Data([trace])
     else:
-        trace1 = go.Scatter(x = labeled[:,axis[0]], y = labeled[:,axis[1]],
-                 mode = 'markers',
-                 marker=dict(size=8))
+        trace1 = go.Scatter(x=labeled[:, axis[0]], y=labeled[:, axis[1]],
+                            mode='markers',
+                            marker=dict(size=8))
         data = go.Data([trace, trace1])
     fig = go.Figure(data=data, layout=layout)
     # Plot and embed in ipython notebook!
     iplot(fig, filename='basic-scatter')
 
-    
-def visualize_point_3D(X, axis=[0,1,2], labeled=None, savefig=False, filename=None):
+
+def visualize_point_3D(X, axis=[0, 1, 2], labeled=None, savefig=False, filename=None):
     '''
     Visualize high dimensional data in 3D according by projection.
     Input:
@@ -84,20 +86,20 @@ def visualize_point_3D(X, axis=[0,1,2], labeled=None, savefig=False, filename=No
     Output:
     - visualization of the data in 3D
     '''
-    trace = go.Scatter3d(x=X[:,axis[0]], y=X[:,axis[1]], z=X[:,axis[2]],
-                    mode='markers',
-                    marker=dict(size=2,
-                        line=dict(color='rgba(217, 217, 217, 0.14)', width=0.5),
-                        opacity=0.8))
+    trace = go.Scatter3d(x=X[:, axis[0]], y=X[:, axis[1]], z=X[:, axis[2]],
+                         mode='markers',
+                         marker=dict(size=2,
+                                     line=dict(
+                                         color='rgba(217, 217, 217, 0.14)', width=0.5),
+                                     opacity=0.8))
     if labeled is None:
         data = go.Data([trace])
     else:
-        trace1 = go.Scatter3d(x=labeled[:,axis[0]], y=labeled[:,axis[1]], z=labeled[:,axis[2]],
-                    mode='markers',
-                    marker=dict(size=5,
-                        line=dict(color='rgba(217, 217, 217, 0.14)', width=0.5)))
+        trace1 = go.Scatter3d(x=labeled[:, axis[0]], y=labeled[:, axis[1]], z=labeled[:, axis[2]],
+                              mode='markers',
+                              marker=dict(size=5,
+                                          line=dict(color='rgba(217, 217, 217, 0.14)', width=0.5)))
         data = go.Data([trace, trace1])
     layout = go.Layout(margin=dict(l=0, r=0, b=0, t=0))
     fig = go.Figure(data=data, layout=layout)
     iplot(fig, filename='simple-3d-scatter')
-    
